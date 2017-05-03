bc78ba5fea8a3221bba5f059b5b1775916d4f385
XavierGuichet <contact@xavier-guichet.fr>
2017-05-03 16:35:07 +0200

    Don't know what i was trying to do, but this messed up everything Keeped commented in case, i figure it

--- src/Bemoove/AppBundle/BemooveAppBundle.php
+++ src/Bemoove/AppBundle/BemooveAppBundle.php
@@ -8,15 +8,15 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BemooveAppBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-        $container->addCompilerPass(new class implements CompilerPassInterface {
-            public function process(ContainerBuilder $container) {
-                $container
-                    ->findDefinition('api_platform.listener.request.deserialize')
-                    ->clearTags();
-            }
-        });
-    }
+    // public function build(ContainerBuilder $container)
+    // {
+    //     parent::build($container);
+    //     $container->addCompilerPass(new class implements CompilerPassInterface {
+    //         public function process(ContainerBuilder $container) {
+    //             $container
+    //                 ->findDefinition('api_platform.listener.request.deserialize')
+    //                 ->clearTags();
+    //         }
+    //     });
+    // }
 }
